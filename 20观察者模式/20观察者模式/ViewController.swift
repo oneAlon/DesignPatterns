99f8baec63b10752aadfdc4b993d83be72b170db
xygj <wangyalong24@126.com>
2018-11-23 16:38:37 +0800

    观察者模式

---
+++ 20观察者模式/20观察者模式/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20观察者模式
+//
+//  Created by xygj on 2018/11/23.
+//  Copyright © 2018 xygj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
