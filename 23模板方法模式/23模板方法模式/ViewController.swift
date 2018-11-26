a7b727e6d2047430462b3f68b272e86b8430e52c
xygj <wangyalong24@126.com>
2018-11-26 19:36:29 +0800

    访问者模式

---
+++ 23模板方法模式/23模板方法模式/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  23模板方法模式
+//
+//  Created by xygj on 2018/11/26.
+//  Copyright © 2018 xygj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
---
+++ 24访问者模式/24访问者模式/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  24访问者模式
+//
+//  Created by xygj on 2018/11/26.
+//  Copyright © 2018 xygj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
