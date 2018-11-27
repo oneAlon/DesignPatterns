1afc44e23554bfe3bb44b882833b311abb788437
xygj <wangyalong24@126.com>
2018-11-27 14:38:56 +0800

    单例模式

--- 5单例模式/5单例模式/ViewController.swift
+++ 5单例模式/5单例模式/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        let signleton1 = YASignleton.default
+        let signleton2 = YASignleton.default
+        let signleton3 = YASignleton.default
+        let signleton4 = YASignleton()
+        
+        print(signleton1, signleton2, signleton3, signleton4)
+        
+        
     }
 
 
