610a5f144792ff2c850f341725873817ea527a90
xygj <wangyalong24@126.com>
2018-11-27 17:05:43 +0800

    单例模式

--- 5单例模式/5单例模式/ViewController.swift
+++ 5单例模式/5单例模式/ViewController.swift
@@ -8,22 +8,29 @@
 
 import UIKit
 
+/*
+ 单例优点:
+    提供对唯一实例的访问, 资源共享
+ 
+ 缺点:
+    单例没有抽象层, 扩展困难
+    单例职责过重, 违反单一职责原则
+    单例的滥用会带来一些负面问题, 资源问题
+ */
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         let signleton1 = YASignleton.default
         let signleton2 = YASignleton.default
         let signleton3 = YASignleton.default
-        let signleton4 = YASignleton()
+        let signleton4 = YASignleton.init()
         
         print(signleton1, signleton2, signleton3, signleton4)
         
-        
     }
-
 
 }
 
